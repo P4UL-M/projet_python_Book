e3745a0b145dc7797c305064456abfc896207ee0
P4UL-M <paul.mairesse@free.fr>
2021-11-29 09:40:30 +0100

    get user in connection portal

--- lib/preset_fonction_IHM.py
+++ lib/preset_fonction_IHM.py
@@ -1,5 +1,7 @@
+from os import name
 import tkinter as tk
 import tkinter.ttk as ttk
+from lib.users_functions import *
 
 def get_connection(master:tk.Frame,on_close = None):
     win = tk.Toplevel(master)
@@ -19,13 +21,21 @@ def get_connection(master:tk.Frame,on_close = None):
         if on_close != None:
             on_close()
         win.destroy()
-    
+
     center = ttk.Frame(main)
     center.place(relx=0.5, rely=0.5, anchor="center")
-    name = ttk.Entry(center)
-    name.grid(column=0,row=0)
-    btn = ttk.Button(center,text="Connect")
+    name_widget = ttk.Entry(center)
+    name_widget.grid(column=0,row=0)
+
+    def handledata(e=None):
+        name = name_widget.get()
+        if name:
+            print(get_reader(name))
+
+    btn = ttk.Button(center,text="Connect",command=handledata)
     btn.grid(column=0,row=1,sticky="we")
 
+    name_widget.bind("<Return>",handledata)
+    name_widget.bind("<")
     win.bind("<FocusOut>", on_focus_out)
     win.protocol("WM_DELETE_WINDOW", on_closing)
