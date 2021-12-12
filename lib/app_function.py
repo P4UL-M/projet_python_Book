3ef63bb50e9437c5a35f26b65abef9af10f5da54
P4UL-M <paul.mairesse@free.fr>
2021-12-12 21:40:31 +0100

    Fix on mousewheel portation on windows and dinamic speed

--- lib/app_function.py
+++ lib/app_function.py
@@ -19,11 +19,12 @@ def get_gallery(parent:tk.Frame,parent_scroll:dict=None):
     _gal["frame_id"] = _gal["canvas"].create_window((0,0),window=_gal["frame"],anchor="nw")
 
     def config(e):
+        _gal["coef_scroll"] = _gal["frame"].winfo_width()/parent.winfo_width()*40
         _gal["canvas"].configure(scrollregion = _gal["canvas"].bbox('all'))
         _gal["canvas"].itemconfig(_gal["frame_id"], height = e.height)
     def _on_mousewheel(event):
-        if event.state:
-            offset = -event.delta/120
+        if event.state%2:
+            offset = -event.delta/(_gal["coef_scroll"] * abs(event.delta))
             _gal["scroll_pos"] += offset if (_gal["scroll_pos"]+offset > 0 and _gal["scroll_pos"]+offset < 1) else 0
             _gal["canvas"].xview(MOVETO,_gal["scroll_pos"])
         else:
@@ -68,41 +69,6 @@ def get_gallery(parent:tk.Frame,parent_scroll:dict=None):
     
     return _gal
 
-def get_horizontale_scroll_bar(parent:ttk.Frame,parent_scroll:dict=None):
-    _dic = {}
-
-    _dic["canvas"] = tk.Canvas(parent,bd=0, highlightthickness=0)
-    _dic["canvas"].pack(expand=1,fill="both")
-
-    _dic["frame"] = ttk.Frame(parent)
-    _dic["frame_id"] = _dic["canvas"].create_window((0,0),window=_dic["frame"],anchor="nw")
-
-    def config(e):
-        _dic["canvas"].configure(scrollregion = _dic["canvas"].bbox('all'))
-        _dic["canvas"].itemconfig(_dic["frame_id"], height = e.height)
-
-    def _on_mousewheel(event):
-        if event.state:
-            _dic["canvas"].xview_scroll(event.delta, "units")
-        else:
-            if parent_scroll:
-                parent_scroll["_on_mousewheel"](event)
-    def _bound_to_mousewheel(e):
-        _dic["canvas"].bind_all("<MouseWheel>", _on_mousewheel)
-    def _unbound_to_mousewheel(e):
-        if parent_scroll:
-            _dic["canvas"].bind_all("<MouseWheel>", parent_scroll["_on_mousewheel"])
-        else:
-            _dic["canvas"].unbind_all("<MouseWheel>")
-    
-    _dic["_on_mousewheel"] = _on_mousewheel
-
-    _dic["canvas"].bind('<Configure>',config)
-    _dic["frame"].bind('<Enter>', _bound_to_mousewheel)
-    _dic["frame"].bind('<Leave>', _unbound_to_mousewheel)
-
-    return _dic
-
 def get_vertical_scroll_bar(parent:ttk.Frame,parent_scroll:dict=None):
     _dic = {}
 
@@ -125,14 +91,15 @@ def get_vertical_scroll_bar(parent:ttk.Frame,parent_scroll:dict=None):
     def config(e):
         if "offset" in _dic.keys():
             _dic.pop("offset")
+        _dic["coef_scroll"] = _dic["frame"].winfo_width()/parent.winfo_width()*40
         _dic["canvas"].configure(scrollregion = _dic["canvas"].bbox('all'))
         _dic["canvas"].itemconfig(_dic["frame_id"], width = e.width)
     def _on_mousewheel(event):
         if "offset" not in _dic.keys():
             # to get the perfect value of the actual position i update it with the position of the scroll bar but it has a small offset between th two value
             _dic["offset"] = _dic["scrollbar"].get()[1]
-        if not event.state:
-            offset = -event.delta/120
+        if not event.state%2:
+            offset = -event.delta/(_dic["coef_scroll"] * abs(event.delta))
             _dic["scroll_pos"] = _dic["scrollbar"].get()[1] - _dic["offset"]
             _dic["scroll_pos"] += offset
             _dic["canvas"].yview(MOVETO,_dic["scroll_pos"])
